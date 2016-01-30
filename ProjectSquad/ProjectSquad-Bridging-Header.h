dabb6b65e1cb6f0c176227045b09748e712503a3
Andrew Pang <andrewpang94@gmail.com>
2016-01-30 15:16:03 -0800

    Added Facebook login framework

---
+++ ProjectSquad/ProjectSquad-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  ProjectSquad-Bridging-Header.h
+//  ProjectSquad
+//
+//  Created by Andrew Pang on 1/30/16.
+//  Copyright © 2016 Squad Up App. All rights reserved.
+//
+
+#ifndef ProjectSquad_Bridging_Header_h
+#define ProjectSquad_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+#endif /* ProjectSquad_Bridging_Header_h */
--- ProjectSquad/ViewController.swift
+++ ProjectSquad/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            // User is already logged in, do work such as go to next view controller.
+        }
+        else
+        {
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "email", "user_friends"]
+            loginView.delegate = self
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        
+    }
+    
+    func loginButtonWillLogin(loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
 
 }
 
